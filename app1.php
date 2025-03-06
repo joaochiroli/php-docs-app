18cbd85e862806962c13051178842890d1630180
João Chiroli <36668502+joaochiroli@users.noreply.github.com>
2025-03-06 10:42:58 -0300

    Create app1.php

---
+++ app1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App 1 - Produção</title>
+</head>
+<body style="text-align: center; font-family: Arial, sans-serif;">
+    <h1>Este é o App 1 - Produção</h1>
+    <p>Versão atual: <strong>1.0</strong></p>
+    <p>Rodando no host: <strong><?php echo gethostname(); ?></strong></p>
+</body>
+</html>
